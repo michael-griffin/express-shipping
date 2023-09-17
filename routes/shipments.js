c5250f904aedd5ab494a3061b22e697d8042a342
Michael <michaellngriffin@gmail.com>
2023-09-17 16:31:51 -0400

    added schema/tests for it, pre-review

--- routes/shipments.js
+++ routes/shipments.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schemas/shippingschema.json");
+
 const express = require("express");
 const { BadRequestError } = require("../expressError");
 const router = new express.Router();
@@ -12,12 +15,18 @@ const { shipProduct } = require("../shipItApi");
  *   { productId, name, addr, zip }
  *
  * Returns { shipped: shipId }
+ *
+ * if invalid, returns a list of errors where body did not match schema
  */
-
 router.post("/", async function (req, res, next) {
-  if (req.body === undefined) {
-    throw new BadRequestError();
+  const result = jsonschema.validate(
+    req.body, orderSchema, {required: true});
+
+  if (!result.valid){
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
   }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
@@ -25,3 +34,14 @@ router.post("/", async function (req, res, next) {
 
 
 module.exports = router;
+
+
+//
+// router.post("/", async function (req, res, next) {
+//   if (req.body === undefined) {
+//     throw new BadRequestError();
+//   }
+//   const { productId, name, addr, zip } = req.body;
+//   const shipId = await shipProduct({ productId, name, addr, zip });
+//   return res.json({ shipped: shipId });
+// });
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -4,6 +4,7 @@ const request = require("supertest");
 const app = require("../app");
 
 
+/** tests post route for successful post, invalid schema, and empty body */
 describe("POST /", function () {
   test("valid", async function () {
     const resp = await request(app).post("/shipments").send({
@@ -22,4 +23,23 @@ describe("POST /", function () {
       .send();
     expect(resp.statusCode).toEqual(400);
   });
+
+  test("throws error if input to API does not match schema", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        productId: 999,
+        name: 12,
+        addr: 100,
+        zip: "12",
+      });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual( {"error":
+      {"message": ["instance.productId must be greater than or equal to 1000",
+        "instance.name is not of a type(s) string",
+        "instance.addr is not of a type(s) string",
+        "instance.zip does not meet minimum length of 5"],
+      "status": 400}});
+  })
 });
